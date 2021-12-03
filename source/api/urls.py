24338f00bf30cd71194ac14d7743f89a8e13d561
ML500 <emilrakaev@gmail.com>
2021-12-03 12:16:30 +0600

    Update name of path

--- source/api/urls.py
+++ source/api/urls.py
@@ -5,7 +5,7 @@ from api.views import AuthViewSet, AmocrmRequest
 router = DefaultRouter()
 router.register(r'auth', AuthViewSet, basename='auth')
 urlpatterns = [
-    path('update_or_create/', AmocrmRequest.as_view({'get': 'list'})),
+    path('amocrm_request/', AmocrmRequest.as_view({'get': 'list'})),
 ]
 
 urlpatterns += router.urls
